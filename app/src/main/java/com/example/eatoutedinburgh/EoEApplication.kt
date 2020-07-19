bf620b2d0c220c53a03f4b0eb8e553edc995696e
yossaj <yossaj@icloud.com>
2020-07-19 19:27:27 +0100

    and everyone else

---
+++ app/src/main/java/com/example/eatoutedinburgh/EoEApplication.kt
@@ -0,0 +1,10 @@
+package com.example.eatoutedinburgh
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EoEApplication  : Application(){
+
+
+}
---
+++ app/src/main/java/com/example/eatoutedinburgh/di/RetrofitModule.java
@@ -0,0 +1,5 @@
+package com.example.eatoutedinburgh.di;
+
+public class RetrofitModule {
+
+}
