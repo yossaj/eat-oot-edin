e4448995d7df424582bc631d97172f34d5866b9c
yossaj <yossaj@icloud.com>
2020-07-20 07:14:50 +0100

    basic navigation setup

---
+++ app/src/main/java/com/example/eatoutedinburgh/ui/main/FrontPage.kt
@@ -0,0 +1,25 @@
+package com.example.eatoutedinburgh.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.eatoutedinburgh.R
+
+class FrontPage : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_front_page, container, false)
+    }
+
+}
