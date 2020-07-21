fe30ee05f3ec47ed8a137a295e33caf5797b58eb
yossaj <yossaj@icloud.com>
2020-07-21 20:28:56 +0100

    image placeholder added for empty images

--- app/src/main/java/com/example/eatoutedinburgh/ui/main/RestaurantFrontViewHolder.kt
+++ app/src/main/java/com/example/eatoutedinburgh/ui/main/RestaurantFrontViewHolder.kt
@@ -1,6 +1,9 @@
 package com.example.eatoutedinburgh.ui.main
 
+import android.graphics.Color
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.eatoutedinburgh.R
 import com.example.eatoutedinburgh.data.models.Restaurant
 import com.example.eatoutedinburgh.databinding.RestaurantItemCardBinding
 import com.squareup.picasso.Picasso
@@ -9,8 +12,13 @@ class RestaurantFrontViewHolder(val binding : RestaurantItemCardBinding) : Recyc
 
     fun bind(restaurant : Restaurant){
         binding.restaurant = restaurant
+        binding.restaurantThumbnail.scaleType = ImageView.ScaleType.CENTER_CROP
         if( !restaurant.thumbnail.isNullOrBlank()){
             Picasso.get().load(restaurant.thumbnail).into(binding.restaurantThumbnail)
+        }else{
+            binding.restaurantThumbnail.scaleType = ImageView.ScaleType.CENTER
+            binding.restaurantThumbnail.setBackgroundColor(Color.WHITE)
+            Picasso.get().load(R.drawable.fork).into(binding.restaurantThumbnail)
         }
 
     }
