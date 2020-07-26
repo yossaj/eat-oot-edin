199682d8543a34d31dd39f85147f71117b64d94e
yossaj <yossaj@icloud.com>
2020-07-26 23:15:42 +0100

    the worst scroll animation in the world

--- app/src/main/java/com/example/eatoutedinburgh/ui/main/FrontPageFragment.kt
+++ app/src/main/java/com/example/eatoutedinburgh/ui/main/FrontPageFragment.kt
@@ -1,13 +1,24 @@
 package com.example.eatoutedinburgh.ui.main
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.graphics.Color
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
+import com.example.eatoutedinburgh.R
 import com.example.eatoutedinburgh.databinding.FragmentFrontPageBinding
 import com.example.eatoutedinburgh.viewmodels.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -49,8 +60,46 @@ class FrontPageFragment : Fragment() {
         viewModel.restaurants.observe(viewLifecycleOwner, Observer { restaurants ->
            restaurantAdapter.submitList(restaurants)
         })
-
+        shrinkRecyclerViewOnScroll(binding)
         return binding.root
     }
+
+    fun shrinkRecyclerViewOnScroll(binding: FragmentFrontPageBinding){
+
+        val height = binding.collectionRecyclerView.height.toFloat()
+        val moveUp = ObjectAnimator.ofFloat(binding.collectionRecyclerView, View.TRANSLATION_Y, -height)
+        var fadeOut = ObjectAnimator.ofArgb(binding.collectionRecyclerView,
+            "backgroundColor", Color.WHITE, Color.TRANSPARENT)
+//                    animator.repeatCount = 1
+//                    animator.repeatMode = ObjectAnimator.REVERSE
+//                    animator.disableViewDuringAnimation(translateButton)
+        val set = AnimatorSet()
+        set.playTogether(moveUp, fadeOut)
+        set.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                binding.collectionRecyclerView.visibility = View.GONE
+
+            }
+        })
+
+        binding.restaurantRecyclerView.addOnScrollListener(object : OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if(dy > 1){
+                    set.start()
+                }else if(dy < -1){
+                    binding.collectionRecyclerView.visibility = View.VISIBLE
+                    moveUp.repeatCount = 1
+                    moveUp.repeatMode = ObjectAnimator.REVERSE
+                    fadeOut.repeatCount = 1
+                    fadeOut.repeatCount = ObjectAnimator.REVERSE
+                    set.start()
+                }
+            }
+
+        })
+    }
+
+
 
 }
