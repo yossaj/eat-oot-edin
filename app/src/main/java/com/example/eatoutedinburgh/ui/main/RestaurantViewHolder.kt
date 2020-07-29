788fa40268d3b33a2b3f6d46971b76f9928536ba
yossaj <yossaj@icloud.com>
2020-07-29 11:38:24 +0100

    change to image placeholder cropping

--- app/src/main/java/com/example/eatoutedinburgh/ui/main/RestaurantViewHolder.kt
+++ app/src/main/java/com/example/eatoutedinburgh/ui/main/RestaurantViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.eatoutedinburgh.ui.main
 
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eatoutedinburgh.R
 import com.example.eatoutedinburgh.data.models.Restaurant
@@ -11,14 +12,16 @@ class RestaurantViewHolder(val binding : RestaurantItemCardsBinding) : RecyclerV
     fun bind(restaurant : Restaurant){
         binding.restaurant = restaurant
         if(restaurant.thumbnail.isNotBlank() && restaurant.id > 0){
+            binding.restaurantThumbnail.scaleType = ImageView.ScaleType.CENTER_CROP
             Picasso.get().load(restaurant.thumbnail).into(binding.restaurantThumbnail)
         }else if(restaurant.id < 0){
+            binding.restaurantThumbnail.scaleType = ImageView.ScaleType.CENTER_CROP
             val drawable = restaurant.thumbnail
             Picasso.get().load(drawable).into(binding.restaurantThumbnail)
             binding.restaurantArea.text = restaurant.url
-
         }
         else{
+            binding.restaurantThumbnail.scaleType = ImageView.ScaleType.CENTER
             Picasso.get().load(R.drawable.fork).into(binding.restaurantThumbnail)
         }
     }
